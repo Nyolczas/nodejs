a7e79b26781381e4518531a6144d499cfb46e720
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-03-08 15:08:00 +0100

    statikus tartalom kiszolgálása

--- server/mymodule.js
+++ server/mymodule.js
@@ -1,7 +1,47 @@
 // Szükséges modulok beolvasása
-var itfactory = require("itfactory");
+var itfactory = require("itfactory"),
+    http = require("http"),
+    fs = require("fs"),
+    port = 3333;
 
 // Termékek lekérése a file-rendszerből
-var products = itfactory.find('json/products.json', 'name', 'Borotva', 'utf8');
+var products = itfactory.find('json/products.json', 'name', 'otva', 'utf8');
 
 itfactory.showConsoleMessage(products);
+
+// Get kérések kiszolgálása
+function handleGetRequest(request, response) {
+
+    var url = "." + request.url;
+
+    // Hibatűrő filekeresés
+    try {
+        var file = fs.readFileSync(url, "utf8");
+
+        response.end(file);
+        
+    } catch (e) {
+        //
+    }
+    console.log(url);
+
+}
+
+// Szerver indítása
+var app = http.createServer(function (request, response) {
+
+    response.setHeader('Access-Control-Allow-Origin', '*'); // fejléc beállítása, hogy más domainről is el lehessen érni
+
+    //Kérések kezelése
+    switch (request.method.toLowerCase()) {
+        case "get":
+            handleGetRequest(request, response);
+            break;
+    }
+
+    console.log("Kérés metódus: ", request.method);
+    console.log(request.url);
+
+    response.end("Hello...");
+
+}).listen(port);
